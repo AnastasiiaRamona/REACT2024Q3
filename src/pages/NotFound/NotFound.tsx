c95980bc3319ce64b978ee4da10476bf1d34b1bb
Anastasia <anastasia@MacBook-Air-Anastasia.local>
2024-08-16 12:15:50 +0200

    feat: add 404 page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,18 @@
+import imageSrc from '../../assets/404.webp';
+import Button from '../../components/Button/Button';
+import styles from './NotFound.module.css';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>404 - Page Not Found</h1>
+      <img src={imageSrc} alt='404' className={styles.image} />
+      <div className={styles.textContainer}>
+        <p className={styles.text}>This is not the page you are looking for</p>
+        <Button text='Go Back' onClick={() => window.history.back()} />
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
